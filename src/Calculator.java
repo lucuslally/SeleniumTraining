51d0bc5dd2b223d477c475c630e0aca97b0aab47
Lucus Lally <lucuslally1@gmail.com>
2018-04-27 11:48:19 -0700

    Calculator Class

---
+++ src/Calculator.java
@@ -0,0 +1,49 @@
+public class Calculator {
+    /*
+    Calculator
+    1. Methods - Add, sub, mul, div
+    2. return - int
+    3. parameters - each have two int
+     */
+
+    static int x = 10;
+    static int y = 2;
+
+    public static void main(String[] args){
+        Calculator calc = new Calculator();
+        calc.add(x, y); // expect 12
+
+        calc.subtract(x,y); // expect 8
+
+        calc.multiply(x,y); // expect 20
+
+        calc.divide(x,y); // expect 5
+
+    }
+
+    public int add(int x, int y){
+        int results = x + y;
+
+        System.out.println(results);
+        return results;
+    }
+    public int subtract(int x, int y){
+        int results = x - y;
+
+        System.out.println(results);
+        return results;
+    }
+    public int multiply(int x, int y){
+        int results = x * y;
+
+        System.out.println(results);
+        return results;
+    }
+    public int divide(int x, int y){
+        int results = x / y;
+
+        System.out.println(results);
+        return results;
+    }
+
+}
