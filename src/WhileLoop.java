275a847e9a8168d9667457a39f2b28ab85eba294
Lucus Lally <lucuslally1@gmail.com>
2018-04-28 22:04:11 -0700

    Modified Loop Classes

--- src/WhileLoop.java
+++ src/WhileLoop.java
@@ -11,25 +11,15 @@ public class WhileLoop {
 
 	public static void main(String[] args) {
 
-		int i=11;
-		
-		while(true){
-			
-			i++;
-			
-		}
-		
-		/*do{
-			
-			System.out.println(i);
-			i++;
-			
-		}
-		while(i<=10);*/
-		//System.out.println(i);
-		//System.out.println("After loop");
-		
+        int i = 1;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while (i <= 10);
+        System.out.println(i); 
+        System.out.println("After loop");
 
-	}
 
+    }
 }
