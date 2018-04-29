62eb3a07d1f244b4b0201fea793dba28a9b38bdd
Lucus Lally <lucuslally1@gmail.com>
2018-04-28 18:31:54 -0700

    BreakandCont, ForLoop, NestedLoops, WhileLoop Classes added

---
+++ src/ForLoop.java
@@ -0,0 +1,28 @@
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+
+		/*
+		 * 
+		 * while --> when the number of iterations are unknown
+		 * For --> number of iterations are known
+		 * do While --> at least for one time irrespective of the condition
+		 * 
+		 * 
+		 * 
+		 */
+
+		
+		
+		for(int i=1;i<=10 ;i++){
+			
+			
+			System.out.println(i);
+			i++;
+			
+		}
+
+	}
+
+}
---
+++ src/WhileLoop.java
@@ -0,0 +1,35 @@
+
+public class WhileLoop {
+	
+	/*
+	 *  loops are not statements --> they are not terminated but exits
+	 *  entry criteria --> loop condition must be true
+	 *  exist criteria --> loop condition is False
+	 * 
+	 * 
+	 */
+
+	public static void main(String[] args) {
+
+		int i=11;
+		
+		while(true){
+			
+			i++;
+			
+		}
+		
+		/*do{
+			
+			System.out.println(i);
+			i++;
+			
+		}
+		while(i<=10);*/
+		//System.out.println(i);
+		//System.out.println("After loop");
+		
+
+	}
+
+}
