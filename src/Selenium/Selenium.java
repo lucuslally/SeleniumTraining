29f7ab10a28cffb1218a5c6115910b97df994707
Lucus Lally <lucuslally1@gmail.com>
2018-04-30 19:58:54 -0700

    Tested Selenium FireFox & Chrome WebDriver

--- src/Selenium/Selenium.java
+++ src/Selenium/Selenium.java
@@ -1,2 +1,40 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
 public class Selenium {
+
+    public static void main(String[] args) {
+        // Chrome
+        //System.setProperty("webdriver.chrome.driver", "/Users/llally/Documents/Java/chromedriver");
+        //WebDriver driver = new ChromeDriver();
+        //driver.get("http://wwww.reddit.com");
+
+        //FireFox
+
+        System.setProperty("webdriver.gecko.driver", "/Users/llally/Documents/Java/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.misspnw.com/");
+        System.out.println(driver.getTitle());
+
+        /*
+        WebElement home = driver.findElement(By.id("menu-item-20"));
+        home.click();
+
+        //home = driver.findElement(By.name(""));
+        //home = driver.findElement(By.xpath());
+        System.out.println(home.getText());
+        */
+
+        //Find the 5 Things to do in San Juan post
+        WebElement thingsToDoInSanJuan = driver.findElement(By.id("post-997"));
+        //Click it
+        thingsToDoInSanJuan.click();
+
+
+
+    }
 }
