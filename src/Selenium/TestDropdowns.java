ccc4f9d21aeebb8853043ccca962f98ac76a8110
Lucus Lally <lucuslally1@gmail.com>
2018-05-02 14:09:26 -0700

    TestDropdowns Class

--- src/Selenium/TestDropdowns.java
+++ src/Selenium/TestDropdowns.java
@@ -3,8 +3,6 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
 
 public class TestDropdowns {
 
